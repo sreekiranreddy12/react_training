9c83f4dc91474e79ca3fa8ac9fa459c4c6a7bd94
sreekiranreddy12 <sreekiran122004@gmail.com>
2025-02-06 12:05:40 +0530

    assignment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Sidebar1 from './components/Sidebar1'
+import Sidebar2 from './components/Sidebar2'
+import Footer from './components/Footer'
+import './global.css';
+
+
+const App = () =>{
+  return (
+    <div className="app">
+      <Navbar />
+      <Main />
+      <div className='sidebar'>
+      <Sidebar1 />
+      <Sidebar2 />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/components/Sidebar2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar2 = () => {
+  return (
+    <div className='sidebar2'>  
+      Sidebar2
+    </div>
+  )
+}
+
+export default Sidebar2
